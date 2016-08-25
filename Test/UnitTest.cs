99b6f1872a834df247964cc60416cf8dc82a4673
sewa312 <sewa080@mail.ru>
2016-08-26 02:06:14 +0300

    Still testing

--- Test/UnitTest.cs
+++ Test/UnitTest.cs
@@ -33,22 +33,38 @@ namespace Test
             Item signButton = new Item("", "signin-button");
             Item hiddenList = new Item("", "customer-view-hide");
             Item fullForm = new Item("", "edit-policy");
-            Item inform = new Item("", "edit-policy");
             Item generalInfo = new Item("", "section-item");
+
+            Item inform = new Item("", "edit-policy");
+            Item next = new Item("", "next");
+
             TestFunctions.FindItem(signIn).Click();
             TestFunctions.WaitReady();
+            TestFunctions.FindItem(loginField).Clear();
             TestFunctions.FindItem(loginField).SendKeys("david.smith.appulatetest@appulatemail.com");
+            Thread.Sleep(1000);
+            TestFunctions.FindItem(passwordField).Clear();
             TestFunctions.FindItem(passwordField).SendKeys("123321");
+            Thread.Sleep(1000);
             TestFunctions.FindItem(signButton).Click();
             TestFunctions.WaitReady();
             TestFunctions.FindItem(hiddenList).Click();
             TestFunctions.FindItem(fullForm).Click();
-            action.MoveToElement(TestFunctions.FindItem(generalInfo)).Perform();
-            TestFunctions.Driver.FindElementByXPath("//*[contains(text(), 'Additional Information')]").Click();
+            Thread.Sleep(3000);
+            while (!TestFunctions.FindItem(generalInfo).Text.StartsWith("Additional Information"))
+            {
+                TestFunctions.FindItem(next).Click();
+                Thread.Sleep(3000);
+            }
+
+
+            //action.MoveToElement(TestFunctions.Driver.FindElementByXPath("//*[contains(text(), 'General Info')]")).Perform();
+            Thread.Sleep(5000);
+            //TestFunctions.Driver.FindElementByXPath("//*[contains(text(), 'Additional Information')]").Click();
             Thread.Sleep(5000);
             //Test.TestFunctions.Driver.FindElementByClassName(("input__box"));
         }
-
+        
         [TestCleanup]
         public void TearDown()
         {
